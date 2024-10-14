394c10c2e61ddac5d0a39f9d80d8737f70b238b3
Jean <jeanmatheus0612@gmail.com>
2024-10-14 13:24:11 -0300

    Formatando o menu

--- src/App.java
+++ src/App.java
@@ -2,26 +2,31 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        Scanner opcao = new Scanner(System.in);
+        String nome = "Peter Parker";
+        String tipoConta = "Corrente";
+        double saldo = 2500;
+        int opcao = 0;
         
         System.out.println("****************************************");
-        System.out.println("Dados Iniciais do Cliente: ");
-        System.out.println("Nome:               Jean Matheus");
-        System.out.println("Tipo de Conta:      Corrente");
-
-        int saldo = 2500;
-
+        System.out.println("\nDados Iniciais do Cliente: ");
+        System.out.println("\nNome:               " + nome);
+        System.out.println("Tipo de Conta:      " + tipoConta);
         System.out.println("Saldo da Conta:        " + saldo + "R$");
-        System.out.println("****************************************");
+        System.out.println("\n****************************************");
 
-        System.out.println("Operações");
-        System.out.println("1 - Consultar saldos");
-        System.out.println("2 - Receber valor");
-        System.out.println("3 - Transferir valor");
-        System.out.println("4 - Sair");
+        Scanner leitura = new Scanner(System.in);
 
-        System.out.println("Escolha uma opção: ");
-        int escolha = opcao.nextInt();
+        while (opcao != 4){
+            System.out.println("Operações");
+            System.out.println("1 - Consultar saldos");
+            System.out.println("2 - Receber valor");
+            System.out.println("3 - Transferir valor");
+            System.out.println("4 - Sair");
+
+            opcao = leitura.nextInt();
+        }
+        
+
+        
     }
 }
